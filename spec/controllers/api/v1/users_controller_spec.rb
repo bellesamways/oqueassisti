9aecf7ce6c789108dbc06153d2d4124aaf3eba66
Isabelle Samways <belesamways@gmail.com>
2020-09-01 22:52:54 -0300

    adicionando testes para update

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -86,7 +86,6 @@ RSpec.describe Api::V1::UsersController do
       }
 
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(response.body).to have_content "can't be blank"
     end
 
     it 'returns http unprocessable_entity when username is already taken' do
@@ -118,24 +117,146 @@ RSpec.describe Api::V1::UsersController do
   describe 'PATCH #update' do
     before do
       create(:user,
-            name: 'teste legal',
-            email: 'testelegal@teste.com',
-            username: 'testelegal',
-            password: '123teste'
-            )
+             name: 'teste legal',
+             email: 'testelegal@teste.com',
+             username: 'testelegal',
+             password: '123teste')
     end
 
-    it 'returns ok when change the name to a valid name' do
+    it 'returns ok when change the username to a valid username' do
       user = User.first
 
       patch :update, params: { id: user.id, user: {
-            username: 'testezinho',
-            old_password: '123teste'
-          }
-        }
+        username: 'testezinho',
+        old_password: '123teste'
+      } }
 
       expect(response).to have_http_status(:ok)
-      expect(user.reload.username).to eq('testezinho')
+
+      user.reload
+      expect(user.username).to eq('testezinho')
+      expect(BCrypt::Password.new(user.password_digest)).to eq('123teste')
+    end
+
+    it 'returns not ok when change the username to a invalid username because it is not unique' do
+      user = User.first
+
+      create(:user,
+             name: 'testezinho',
+             email: 'testezinho@teste.com',
+             username: 'testezinho',
+             password: '123teste')
+
+      patch :update, params: { id: user.id, user: {
+        username: 'testezinho',
+        old_password: '123teste'
+      } }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+
+      user.reload
+      expect(user.username).to eq('testelegal')
+      expect(BCrypt::Password.new(user.password_digest)).to eq('123teste')
+    end
+
+    it 'returns not ok when change the email to a invalid email because it is not unique' do
+      user = User.first
+
+      create(:user,
+             name: 'testezinho',
+             email: 'testezinho@teste.com',
+             username: 'testezinho',
+             password: '123teste')
+
+      patch :update, params: { id: user.id, user: {
+        email: 'testezinho@teste.com',
+        old_password: '123teste'
+      } }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+
+      user.reload
+      expect(user.email).to eq('testelegal@teste.com')
+      expect(BCrypt::Password.new(user.password_digest)).to eq('123teste')
+    end
+
+    it 'returns ok when change the password correctly' do
+      user = User.first
+
+      expect(BCrypt::Password.new(user.password_digest)).to eq('123teste')
+
+      patch :update_password, params: { id: user.id, user: {
+        old_password: '123teste',
+        password: 'teste123'
+      } }
+
+      expect(response).to have_http_status(:ok)
+
+      user.reload
+
+      expect(BCrypt::Password.new(user.password_digest)).to eq('teste123')
+    end
+
+    it 'returns not ok when change the password incorrectly with blank old_password' do
+      user = User.first
+
+      expect(BCrypt::Password.new(user.password_digest)).to eq('123teste')
+
+      patch :update_password, params: { id: user.id, user: {
+        old_password: '',
+        password: 'teste123'
+      } }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+
+      user.reload
+
+      expect(BCrypt::Password.new(user.password_digest)).to eq('123teste')
+    end
+
+    it 'returns not ok when change the password incorrectly with incorrect old_password' do
+      user = User.first
+
+      expect(BCrypt::Password.new(user.password_digest)).to eq('123teste')
+
+      patch :update_password, params: { id: user.id, user: {
+        old_password: 'teste123',
+        password: '123teste'
+      } }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+
+      user.reload
+
+      expect(BCrypt::Password.new(user.password_digest)).to eq('123teste')
+    end
+
+    it 'returns not ok when change the password incorrectly with blank password' do
+      user = User.first
+
+      expect(BCrypt::Password.new(user.password_digest)).to eq('123teste')
+
+      patch :update_password, params: { id: user.id, user: {
+        old_password: '123teste',
+        password: ''
+      } }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+
+      user.reload
+
+      expect(BCrypt::Password.new(user.password_digest)).to eq('123teste')
     end
   end
+
+  # describe 'DELETE #destroy' do
+  #   before do
+  #     5.times do
+  #       create(:user)
+  #     end
+  #   end
+
+  #   it '' do
+  #   end
+  # end
 end
